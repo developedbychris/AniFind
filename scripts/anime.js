9b99d155a58d1d9da7342e1486538cadc25e5ba4
Chrisu <89726956+ultimatechrisu@users.noreply.github.com>
2022-11-16 09:22:19 -0500

    api call -> localstorage
    script generates html from local storage instead of making an api call.

--- scripts/anime.js
+++ scripts/anime.js
@@ -12,90 +12,90 @@ const options2 = {
 document.querySelector('#goBack-btn').addEventListener('click', ()=> history.back())
 
 const pageID = location.hash.substring(1)
+const params = new URLSearchParams(location.search)
+const animeIndex = parseInt(params.get("a"))
 
+for (let i = 0; i < localStorage.length; i++) {
+  if (i == animeIndex) {continue}
+  localStorage.removeItem(`title${i}`)
+  localStorage.removeItem(`rank${i}`)
+  localStorage.removeItem(`type${i}`)
+  localStorage.removeItem(`id${i}`)
+  localStorage.removeItem(`genre${i}`)
+}
 
+const genAnime = () => {
+  animeWrapper.style.visibility = "visible"
 
-const genAnime = async (animeID) =>{
-    const response = await fetch(`https://anime-db.p.rapidapi.com/anime/by-id/${animeID}`, options2)
-  
-    if(response.status === 200){
-      const anime = await response.json()
-      //Opens Div
-      animeWrapper.style.visibility = 'visible'
+  //CREATING HTML ELEMENTS
 
+  //IMAGE DIV
+  const imageDiv = document.createElement("div")
+  imageDiv.setAttribute("class", " mt-4 p-2 col-12 col-md-6")
+  animeWrapper.appendChild(imageDiv)
+  //TEXT DIV
+  const textDiv = document.createElement("div")
+  textDiv.setAttribute("class", "p-5 col-12 col-md-6 anime text-md-start")
+  animeWrapper.appendChild(textDiv)
 
-      //CREATING HTML ELEMENTS
-      
-      //IMAGE DIV
-      const imageDiv = document.createElement('div')
-      imageDiv.setAttribute('class', ' mt-4 p-2 col-12 col-md-6')
-      animeWrapper.appendChild(imageDiv)
-      //TEXT DIV
-      const textDiv = document.createElement('div')
-      textDiv.setAttribute('class', 'p-5 col-12 col-md-6 anime text-md-start')
-      animeWrapper.appendChild(textDiv)
+  //IMG
+  const img = document.createElement("img")
+  img.src = localStorage.getItem(`image${animeIndex}`)
+  img.setAttribute("class", "anime-img2 mb-4")
+  imageDiv.appendChild(img)
+  //TITLE
+  const animeTitle = document.createElement("p")
+  animeTitle.innerText = localStorage.getItem(`title${animeIndex}`)
+  animeTitle.setAttribute("class", "display-4 anime-title text-center")
+  textDiv.appendChild(animeTitle)
+  //Japanese Title
+  const regex = /[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf]/
+  const japaneseTitle = document.createElement("p")
+  japaneseTitle.innerText = localStorage.getItem(`jp-title${animeIndex}`)
+  japaneseTitle.setAttribute("class", "h4 j-title text-center mb-4")
+  textDiv.appendChild(japaneseTitle)
+  //MAL RANK
+  const rank = document.createElement("p")
+  rank.setAttribute("class", "h5 anime-subheader")
+  if (localStorage.getItem(`rank${animeIndex}`) == 0) {
+    rank.innerText = `MAL Rank: N/A`
+  } else {
+    rank.innerText = `MAL Rank: ${localStorage.getItem(`rank${animeIndex}`)}`
+  }
+  textDiv.appendChild(rank);
+  //GENRES
+  const genres = document.createElement("p")
+  genres.setAttribute("class", "h5 anime-subheader")
+  if (localStorage.getItem(`genre${animeIndex}`).length === 0) {
+    genres.innerText = `Genre: N/A`
+  } else {
+    genres.innerText = `Genres: ${localStorage.getItem(`genre${animeIndex}`)}`
+  }
+  textDiv.appendChild(genres);
+  //EPISODES
+  const episodes = document.createElement("p")
+  episodes.setAttribute("class", "h5 anime-subheader")
+  episodes.innerText = `Episodes: ${localStorage.getItem(`episodes${animeIndex}`)}`
+  textDiv.appendChild(episodes);
+  //Type
+  const aniType = document.createElement("p")
+  aniType.innerText = `Type: ${localStorage.getItem(`type${animeIndex}`)}`
+  aniType.setAttribute("class", "h5 anime-subheader")
+  textDiv.appendChild(aniType)
+  //MAL Page Button
+  const malBtn = document.createElement("button")
+  malBtn.innerText = `MAL Page`
+  malBtn.setAttribute("class", "btn btn-info mt-2 mb-4");
+  malBtn.addEventListener("click", () => parent.open(localStorage.getItem(`mal-link${animeIndex}`)))
+  textDiv.appendChild(malBtn)
+  //Synopsis
+  const synop = document.createElement("p")
+  synop.innerText = localStorage.getItem(`synopsis${animeIndex}`)
+  synop.classList.add("synop")
+  textDiv.appendChild(synop)
 
-      //IMG
-      const img = document.createElement('img')
-      img.src = anime.image
-      img.setAttribute('class', 'anime-img2 mb-4')
-      imageDiv.appendChild(img)
-      //TITLE
-      const animeTitle = document.createElement('p')
-      animeTitle.innerText = anime.title
-      animeTitle.setAttribute('class', 'display-4 anime-title text-center')
-      textDiv.appendChild(animeTitle)
-      //Japanese Title
-      const regex = /[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf]/
-      const japaneseTitle = document.createElement('p')
-      japaneseTitle.innerText = anime.alternativeTitles.filter(title => title.match(regex))
-      japaneseTitle.setAttribute('class', 'h4 j-title text-center mb-4')
-      textDiv.appendChild(japaneseTitle)
-      //MAL RANK
-      const rank = document.createElement('p')
-      rank.setAttribute('class','h5 anime-subheader')
-      if(anime.ranking === 0){
-        rank.innerText = `MAL Rank: N/A`
-      } else{
-        rank.innerText = `MAL Rank: ${anime.ranking}`
-      }     
-      textDiv.appendChild(rank)
-      //GENRES
-      const genres = document.createElement('p')
-      genres.setAttribute('class','h5 anime-subheader')
-      if(anime.genres.length == 0){
-        genres.innerText = `Genre: N/A`
-        } else{
-        genres.innerText = `Genre: ${anime.genres.join(', ')}`
-        }
-      textDiv.appendChild(genres)
-      //EPISODES
-      const episodes = document.createElement('p')
-      episodes.setAttribute('class', 'h5 anime-subheader')
-      episodes.innerText = `Episodes: ${anime.episodes}`
-      textDiv.appendChild(episodes)
-      //Type
-      const aniType = document.createElement('p')
-      aniType.innerText = `Type: ${anime.type}`
-      aniType.setAttribute('class','h5 anime-subheader')
-      textDiv.appendChild(aniType)
-      //MAL Page Button
-      const malBtn = document.createElement('button')
-      malBtn.innerText = `MAL Page`
-      malBtn.setAttribute('class', 'btn btn-info mt-2 mb-4')
-      malBtn.addEventListener('click', ()=> parent.open(anime.link))
-      textDiv.appendChild(malBtn)
-      //Synopsis
-      const synop = document.createElement('p')
-      synop.innerText = anime.synopsis
-      synop.classList.add('synop')
-      textDiv.appendChild(synop)
-      
-     
-
-    }
   
 }
 
-genAnime(pageID)
+genAnime();
             
